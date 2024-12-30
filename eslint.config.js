5cb96abe34e4dd7758cc75d0d6fb53c4b0097df5
Henrique Godinho <henrique@hgod.in>
2024-12-30 18:47:29 -0300

    add ESLint rule configuration for React Hooks JSX runtime

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,7 @@ export default tseslint.config(
                 'warn',
                 { allowConstantExport: true },
             ],
+            ...reactHooks.configs['jsx-runtime'].rules,
         },
     },
 )
