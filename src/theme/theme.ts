a7a58bbeb205e7fddfecb4dfd872a9a611372ffb
Henrique Godinho <henrique@hgod.in>
2024-12-30 18:46:29 -0300

    add theme configuration and integrate with Tailwind CSS

---
+++ src/theme/theme.ts
@@ -0,0 +1,7 @@
+export const theme = {
+    fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        mono: ["JetBrains Mono", "monospace"],
+    },
+    extend: {},
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
+import { theme } from "./src/theme"
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
-    },
+    theme,
     plugins: [],
 }
 
