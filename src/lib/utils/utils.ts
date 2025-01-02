19d67980792fec873752c103795e50f253b6298e
Henrique Godinho <henrique@hgod.in>
2025-01-02 20:07:18 -0300

    refactor utility function to accept ClassNameValue type for improved type safety

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -1,6 +1,6 @@
 import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
+import { twMerge, ClassNameValue } from "tailwind-merge";
 
-export function cn(...classes: string[]) {
+export function cn(...classes: ClassNameValue[]) {
     return clsx(twMerge(...classes));
 }
