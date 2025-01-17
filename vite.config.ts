e1185e57716f817f05522b950979f100a7d60a4d
Henrique Godinho <henrique@hgod.in>
2025-01-17 10:46:00 -0300

    update Vite configuration: change server port to 4000 and add preview settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,19 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     server: {
         host: "0.0.0.0",
-        port: 3000,
+        port: 4000,
+    },
+    preview: {
+        host: "0.0.0.0",
+        port: 4001,
     },
     resolve: {
         alias: {
             "@": "/src",
         },
+    },
+    build: {
+        emptyOutDir: true,
     },
     plugins: [react()],
 });
