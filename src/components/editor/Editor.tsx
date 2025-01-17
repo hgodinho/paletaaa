b86c30db12e0784b705cdc7a98235987450dcca7
Henrique Godinho <henrique@hgod.in>
2025-01-17 01:07:06 -0300

    refactor Editor component: simplify state management and integrate ColorGraph for background handling

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,75 +1,23 @@
 import { cn } from "@/lib";
-import {
-    Label,
-    Input,
-    Textarea,
-    ColorType,
-    Header,
-    Picker,
-} from "@/components";
-import { PaletteContextType, usePaletteContext } from "@/context";
-import { useCallback, useEffect, useState } from "react";
-import { parseColor } from "react-aria-components";
+import { usePaletteContext } from "@/context";
+import { ColorGraph } from "@/components";
 
 export function Editor() {
-    const { setPalette, ...palette } = usePaletteContext();
-
-    const handlePaletteName = useCallback(
-        (e: React.ChangeEvent<HTMLInputElement>) => {
-            setPalette((prev: PaletteContextType) => ({
-                ...prev,
-                name: e.target.value,
-            }));
-        },
-        [setPalette]
-    );
-
-    const handleBackgroundColor = useCallback(
-        (color: ColorType) => {
-            setPalette((prev: PaletteContextType) => ({
-                ...prev,
-                background: { value: color.value, title: color.title },
-            }));
-        },
-        [setPalette]
-    );
+    const { contrastColor, getNode } = usePaletteContext();
 
     return (
         <div
-            className={cn(
-                "bg-white",
-                "h-full",
-                "border-r-4",
-                "border-black",
-                "overflow-auto"
-            )}
+            className={cn("editor", "w-full", "h-full", "border-r")}
+            style={{
+                backgroundColor:
+                    getNode("background")?.color.data.toString("hex"),
+                borderColor: contrastColor(
+                    "#fff",
+                    getNode("background")?.color.data.toString("hex")
+                ),
+            }}
         >
-            <Header />
-            <div className={cn("p-3", "flex", "flex-col", "gap-4")}>
-                <Label title={"Palette name"}>
-                    <Input
-                        type="text"
-                        placeholder="Name"
-                        value={palette.name}
-                        onChange={handlePaletteName}
-                    />
-                </Label>
-                <Picker
-                    title="Background"
-                    variant="compact"
-                    color={palette.background}
-                    background={palette.background || { value: "", title: "" }}
-                    onChange={(color) =>
-                        handleBackgroundColor({ value: color, title: "" })
-                    }
-                />
-                <Label title={"Code"}>
-                    <Textarea
-                        value={JSON.stringify(palette, null, 4)}
-                        language="json"
-                    />
-                </Label>
-            </div>
+            <ColorGraph />
         </div>
     );
 }
