0febc22aef6c1063ba67aecf9139ee5eaaddec8d
Henrique Godinho <henrique@hgod.in>
2025-01-21 23:03:51 -0300

    fix(Button): separate exports

---
+++ src/components/button/variant.ts
@@ -0,0 +1,40 @@
+import { cva } from "class-variance-authority";
+
+export const buttonVariants = cva(
+    ["disabled:opacity-50", "disabled:cursor-not-allowed"],
+    {
+        variants: {
+            variant: {
+                default: ["bg-black", "text-white", "p-4"],
+                menu: ["p-2", "w-full", "h-full"],
+                icon: [
+                    "p-2",
+                    "rounded-full",
+                    "hover:bg-gray-200",
+                    "aria-disabled:opacity-50",
+                    "aria-disabled:cursor-not-allowed",
+                ],
+                square: [
+                    "p-2",
+                    "border",
+                    "rounded-sm",
+                    "border",
+                    "border-gray-400",
+                    "hover:bg-gray-200",
+                ],
+                trigger: [
+                    "p-3",
+                    "fixed",
+                    "rounded-full",
+                    "z-20",
+                    "duration-300",
+                    "hover:p-4",
+                ],
+                none: [],
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+        },
+    }
+);
