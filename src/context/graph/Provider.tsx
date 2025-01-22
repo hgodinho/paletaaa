7f29aad2ec590b7e1e0f180f8335e25df72b2a48
Henrique Godinho <henrique@hgod.in>
2025-01-21 23:19:41 -0300

    fix(graph): simplify type declaration

--- src/context/graph/Provider.tsx
+++ src/context/graph/Provider.tsx
@@ -2,7 +2,7 @@ import { useGraph } from "@/lib";
 import { GraphContext } from "./Context";
 import { Node, Link } from "./types";
 
-export function GraphProvider({ children }: React.PropsWithChildren<{}>) {
+export function GraphProvider({ children }: React.PropsWithChildren) {
     const { graph, ...graphActions } = useGraph<Node, Link>();
     return (
         <GraphContext
