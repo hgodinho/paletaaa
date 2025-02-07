8cbeb6dfd9d53ce5a77c1d8f92b1a68e94a7750f
Henrique Godinho <ola@hgod.in>
2025-02-07 15:33:19 -0300

    feat(router): add browser router for future features

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,25 +1,33 @@
 import { AppProvider, OptionsProvider, PaletteProvider } from "@/context";
 import { cn } from "@/lib";
 import { ColorGraph, Menu, Sidebar /*Preview*/ } from "@/components";
+import { BrowserRouter } from "react-router";
 
 export function Layout() {
     return (
-        <AppProvider>
-            <PaletteProvider>
-                <OptionsProvider>
-                    <main
-                        className={cn("h-full", "w-full", "flex", "flex-row")}
-                    >
-                        <Sidebar>
-                            <Menu />
-                        </Sidebar>
-                        <ColorGraph />
-                        {/* <Preview />
+        <BrowserRouter>
+            <AppProvider>
+                <PaletteProvider>
+                    <OptionsProvider>
+                        <main
+                            className={cn(
+                                "h-full",
+                                "w-full",
+                                "flex",
+                                "flex-row"
+                            )}
+                        >
+                            <Sidebar>
+                                <Menu />
+                            </Sidebar>
+                            <ColorGraph />
+                            {/* <Preview />
                             // to be further implemented
                         */}
-                    </main>
-                </OptionsProvider>
-            </PaletteProvider>
-        </AppProvider>
+                        </main>
+                    </OptionsProvider>
+                </PaletteProvider>
+            </AppProvider>
+        </BrowserRouter>
     );
 }
