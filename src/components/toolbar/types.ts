16f14cf44e96f1f7df58d3dd7bca265f1565000c
Henrique Godinho <ola@hgod.in>
2025-02-13 19:13:46 -0300

    refactor(toolbar): to use new tools api

--- src/components/toolbar/types.ts
+++ src/components/toolbar/types.ts
@@ -1,12 +1,3 @@
 import { HTMLAttributes } from "react";
 
-export type ToolsState = {
-    labels: boolean;
-    magnet: boolean;
-};
-
-export type ToolbarProps = HTMLAttributes<HTMLDivElement> & {
-    tools: ToolsState;
-    visible?: boolean;
-    setTools: (tools: ToolsState) => void;
-};
+export type ToolbarProps = HTMLAttributes<HTMLDivElement>;
