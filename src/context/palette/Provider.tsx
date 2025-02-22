0aa536ef614ee21ce5a6ecbf3856a2540c9c0ffd
Henrique Godinho <ola@hgod.in>
2025-02-22 20:39:47 -0300

    feat(pallete.provider): onColorDuplicate method

--- src/context/palette/Provider.tsx
+++ src/context/palette/Provider.tsx
@@ -96,6 +96,33 @@ export function PaletteProvider({
     };
 
     /**
+     * Duplicate a color in the palette
+     * @param id | id of the color
+     *
+     * @posthog color_duplicate event
+     */
+    const onColorDuplicate = (id: string) => {
+        const color = getColor(id);
+        if (!color) return;
+
+        const newId = getRandomId();
+        graphActions.addVertex({
+            id: newId,
+            expanded: true,
+            val: 1.5,
+            color: {
+                ...color,
+                id: newId,
+            },
+        });
+
+        posthog?.capture("color_duplicate", {
+            id,
+            newId,
+        });
+    };
+
+    /**
      * Remove a color from the palette
      * @param id | id of the color
      *
@@ -298,6 +325,7 @@ export function PaletteProvider({
                 getBackgroundHex,
                 contrastColor,
                 onColorAdd,
+                onColorDuplicate,
                 onColorRemove,
                 updateColorName,
                 updateColorData,
